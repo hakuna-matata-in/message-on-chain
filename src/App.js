cd8e73af30e4080e5808d6feca5ebbb99634cb57
dr <75526192+hakuna-matata-in@users.noreply.github.com>
2022-12-21 21:42:37 +0100

    Added the requested change from upwork

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Button, Divider, Input, Page, Slider, Spacer, Text } from "@geist-ui/core";
+import { Button, Divider, Input, Page, Slider, Spacer, Text, Select } from "@geist-ui/core";
 import { ConnectKitButton } from "connectkit";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAccount, useNetwork, useSendTransaction, useWaitForTransaction } from "wagmi";
 import { useDebounce } from 'use-debounce'
 import { usePrepareSendTransaction } from 'wagmi'
@@ -10,7 +10,11 @@ import Tip from "./Tip";
 function App() {
   const { address } = useAccount();
   const { chain } = useNetwork();
-
+  const websiteOwnerPrefix = 'stycap';
+  const [asset, setAsset] = useState('');
+  const assetPredefinedValues = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'GOOG', 'NVDA']
+  const [quantity, setQuantity] = useState('');
+  const [buyOrSell, setBuyOrSell] = useState('');  
   const [message, setMessage] = useState('');
   const [amount, setAmount] = useState('0');
   const [recipient, setRecipient] = useState('');
@@ -25,35 +29,56 @@ function App() {
       data: debouncedMessage ? ethers.utils.hexlify(ethers.utils.toUtf8Bytes(debouncedMessage)) : undefined,
     },
   })
-  
+
   const { data, sendTransaction } = useSendTransaction(config)
- 
+
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
   })
-  
+
+  useEffect(() => {
+    setMessage(`${websiteOwnerPrefix}|${asset}|${buyOrSell}|${quantity}`)
+  }, [asset, quantity, buyOrSell]);
+
   return (
     <Page>
       <Text h1>Message On-Chain</Text>
-      <ConnectKitButton showBalance/>
+      <ConnectKitButton showBalance />
       <Spacer h={0.1} />
       {address && <Text small>Current network: {chain?.name}</Text>}
       <Divider />
       {address && (
-        <div style={{display:"flex", flexDirection: "column"}}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Spacer h={1} />
+          <Text small>Asset</Text>
+          <Select placeholder="Choose one" onChange={(e) => setAsset(e)} width="400px">
+            {
+              assetPredefinedValues.map(asset => <Select.Option value={asset}>{asset}</Select.Option>)
+            }
+          </Select >
+          <Spacer h={1} />
+          <Text small>Buy/Sell</Text>
+          <Select placeholder="Choose one" onChange={(e) => setBuyOrSell(e)} width="400px">
+            <Select.Option value="BUY">BUY</Select.Option>
+            <Select.Option value="SELL">SELL</Select.Option>
+          </Select >
+          <Spacer h={1} />
+          <Text small>Quantity</Text>
+          <Input width='400px' value={quantity} onChange={(e) => setQuantity(e.target.value)} htmlType="number" />
+          <Spacer h={1} />
           <Text small>Message to send</Text>
           <Spacer h={0.1} />
-          <Input width='400px' value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <Input width='400px' value={message} onChange={(e) => setMessage(e.target.value)} disabled/>
           <Spacer h={1} />
           <Text small>Recipient address</Text>
           <Spacer h={0.1} />
-          <Input width='400px' value={recipient} onChange={(e) => setRecipient(e.target.value)}/>
+          <Input width='400px' value={recipient} onChange={(e) => setRecipient(e.target.value)} />
           <Spacer h={1} />
           <Text small>Amount to send (optional) ({chain?.nativeCurrency.symbol})</Text>
           <Spacer h={0.1} />
-          <Input width='400px' value={amount} onChange={(e) => setAmount(e.target.value)}/>
+          <Input width='400px' value={amount} onChange={(e) => setAmount(e.target.value)} />
           <Spacer h={1} />
-          <Button type='success' style={{width:'200px'}} disabled={!message || !recipient} onClick={sendTransaction}>Send Message</Button>
+          <Button type='success' style={{ width: '200px' }} disabled={!message || !recipient} onClick={sendTransaction}>Send Message</Button>
         </div>
       )}
       <Spacer h={1} />
